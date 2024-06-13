24b8e4c7f3b011b201eb73e6e710212a99058f90
Bruka25 <bkteshome862@gmail.com>
2024-06-14 02:15:58 +0300

    python file for having minimum operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,26 +5,31 @@
 
 def minOperations(n):
     """
-    Calculates the fewest number of operations needed to have exactly `n` characters 'H' in a file
-    using only the operations "Copy All" and "Paste".
+    Calculates the fewest number of operations needed to have exactly `n`
+    characters 'H' in a file using only the operations "Copy All" and "Paste".
 
     Args:
     - n (int): The number of 'H' characters desired in the file.
 
     Returns:
-    - int: The minimum number of operations required. Returns 0 if `n` is impossible to achieve.
+    - int: The minimum number of operations required. Returns 0 if `n` is
+           impossible to achieve.
 
     Explanation:
-    - The function uses dynamic programming to build up the minimum operations required for each count of 'H's.
+    - The function uses dynamic programming to build up the minimum operations
+      required for each count of 'H's.
     - Starts with the base case of 1 'H' (0 operations).
-    - For each subsequent count from 2 to `n`, computes the minimum operations by considering all possible ways to achieve
+    - For each subsequent count from 2 to `n`, computes the minimum operations
+      by considering all possible ways to achieve
       that count using previously computed results.
 
     Example:
     >>> minOperations(9)
     6
-    This returns 6 because the minimum operations to achieve 9 'H's is demonstrated in the example provided:
-    H => Copy All => Paste => HH => Paste => HHH => Copy All => Paste => HHHHHH => Paste => HHHHHHHHH
+    This returns 6 because the minimum operations to achieve 9 'H's is
+    demonstrated in the example provided:
+    H => Copy All => Paste => HH => Paste => HHH => Copy All => Paste =>
+    HHHHHH => Paste => HHHHHHHHH
     """
     if not isinstance(n, int):
         return 0
